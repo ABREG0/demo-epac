f73efaca38a4b031cbcacb91c597281c22c0fe50
ABREG0 <ABREG0@ME.com>
2025-02-16 11:56:48 -0800

    set master for module source

--- loops/azapi_call/main.tf
+++ loops/azapi_call/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "this" {
 module "vnets" {
   depends_on = [azurerm_resource_group.this, ]
   for_each   = merge({ for kk, kv in local.vnet_object : kv.name => kv }) #local.creating_nested_objects_vnets2 # {for kk, kv in local.creating_nested_objects_vnets2 : kk => kv }
-  source     = "github.com/elsalvos-org/terraform-azurerm-avm-res-network-virtualnetwork"
+  source     = "github.com/elsalvos-org/terraform-azurerm-avm-res-network-virtualnetwork?ref=master"
 
   name                = each.value.name
   location            = each.value.location
