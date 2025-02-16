b69a0628678ebe89b0185ba242031d7aee1e3068
ABREG0 <ABREG0@ME.com>
2025-02-16 13:56:37 -0800

    fmt

--- loops/azapi_call/main.tf
+++ loops/azapi_call/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "this" {
 
 module "vnets" {
   depends_on = [azurerm_resource_group.this, ]
-  for_each   = merge({ for kk, kv in local.vnet_object : kv.name => kv })                 #local.creating_nested_objects_vnets2 # {for kk, kv in local.creating_nested_objects_vnets2 : kk => kv }
+  for_each   = merge({ for kk, kv in local.vnet_object : kv.name => kv }) #local.creating_nested_objects_vnets2 # {for kk, kv in local.creating_nested_objects_vnets2 : kk => kv }
   source     = "github.com/elsalvos-org/terraform-azurerm-avm-res-network-virtualnetwork?ref=v0.2.0"
   # version    = "v0.2.0"
 
